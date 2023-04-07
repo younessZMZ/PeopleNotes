ac21c72305eafe068ac0a1dbb1fa44d53c19b079
younessZMZ <younes.zmz@gmail.com>
2023-04-07 17:54:13 +0000

    Test black build

---
+++ test_black.py
@@ -0,0 +1,21 @@
+import time
+from random import randint
+
+
+def function(one, two, three):
+    dic = {"hello world we are here and you are there": "We are also here as well and you are not there", "hello me too i'm here": "yes"}
+    if one == 1:
+        counter = 0
+        while counter < 10:
+            random_integer = randint(0, 10)
+            if random_integer < 5:
+                output = f"The random integer is {random_integer} and it is below 5"
+            else:
+                output = f"The random integer is {random_integer} and it is above 5"
+            print(output)
+            counter +=1
+        return True
+    return 'This function did not return True'
+
+if __name__ == '__main__':
+    function(1, 0, 0)
